395fd109e9b1980df8afd4698f21db08651dd4e6
silviapio <silvia.piovesan@gmail.com>
2021-03-15 14:21:04 +0100

    project setup, infinite scrolling working

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { InView } from "react-intersection-observer";
+import { Grid, Row, Col } from "react-flexbox-grid";
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  const [photos, setPhotos] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [count, setCount] = useState(0);
+
+  // eslint-disable-next-line
+  useEffect(() => getPhotos(0, 12), []);
+
+  const getPhotos = (startCount, endCount) => {
+    setIsLoading(true);
+    axios.get(`https://jsonplaceholder.typicode.com/photos?_start=${startCount}&_end=${endCount}`)
+      .then(response => {
+        console.log(response.data);
+        setPhotos([...photos, ...response.data]);
+        setCount(endCount);
+        setIsLoading(false);
+      });
+  };
+
+  const handleLoaderVisible = (visible) => {
+    console.log('Inview:', visible)
+    if (visible) {
+      console.log("getting more photos")
+      getPhotos(count, count + 12)
+    } else {
+      console.log("not getting photos")
+    }
+  }
+
+  return (
+    <div className="container">
+      <h1>Scroll down to load more photos...</h1>
+      <Grid fluid>
+        <Row>
+          {photos.length !== 0 && photos.map((photo, i) => (
+            <Col key={i} xs={6} md={4} lg={3}>
+              <img 
+                src={photo.url} 
+                alt={photo.title}
+                className="img-fluid"
+              />
+            </Col>
+          ))}
+        </Row>
+      </Grid>
+      {(photos.length !== 0 && !isLoading) && 
+        <InView 
+          as="div" 
+          onChange={handleLoaderVisible}>
+        <h2>Loading more...</h2>
+      </InView>}
+    </div>
+
+  );
+}
+
+export default App;
